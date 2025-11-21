7de7b407f3aa74a29903991ee4e415a26c7e1066
Cgun-art <migueloproio903@gmail.com>
2025-11-21 17:47:10 -0300

    Create PATREON REDIRECTER.py

---
+++ files/PATREON REDIRECTER.py
@@ -0,0 +1,6 @@
+import webbrowser
+
+user_input = input(type heads or tails: ).strip().lower(type tails for sure: )
+
+if user_input == "heads" or user_input == "tails":
+    webbrowser.open("https://patreon.com/mtp8484?utm_medium=unknown&utm_source=join_link&utm_campaign=creatorshare_creator&utm_content=copyLink")
