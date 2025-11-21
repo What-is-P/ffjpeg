f8be2d9d9e9984cd01b2efd6df728d7b6c76bf1c
Cgun-art <migueloproio903@gmail.com>
2025-11-21 17:48:15 -0300

    Update PATREON REDIRECTER.py

--- files/PATREON REDIRECTER.py
+++ files/PATREON REDIRECTER.py
@@ -1,6 +1,6 @@
 import webbrowser
 
-user_input = input(type heads or tails: ).strip().lower(type tails for sure: )
+user_input = input(type heads or tails: ).strip(this will redirect you shortly to my patreon).lower(thanks, lol)
 
 if user_input == "heads" or user_input == "tails":
     webbrowser.open("https://patreon.com/mtp8484?utm_medium=unknown&utm_source=join_link&utm_campaign=creatorshare_creator&utm_content=copyLink")
